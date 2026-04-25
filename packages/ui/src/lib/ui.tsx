60a3e55aa6575015500b0443eb144aed2f310432
rijomd <rijomd09@gmail.com>
2026-04-25 16:26:21 +0530

    initial

---
+++ packages/ui/src/lib/ui.tsx
@@ -0,0 +1,15 @@
+
+import styles from './ui.module.css';
+
+export function DevHubUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to DevHubUi!</h1>
+    </div>
+  );
+}
+
+export default DevHubUi;
+
+
+
