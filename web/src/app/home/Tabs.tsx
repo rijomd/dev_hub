6787b951d9e16a705c0cae63848c558cd75d4d22
rijomd <rijomd09@gmail.com>
2026-08-06 20:06:11 +0530

    Task :- added tab component into header

--- web/src/app/home/Tabs.tsx
+++ web/src/app/home/Tabs.tsx
@@ -5,32 +5,30 @@ type TabsProps = {
 
 export const Tabs = ({ activeTab, setActiveTab }: TabsProps) => {
     return (
-        <div  >
-            <div className="bg-[#222222] max-w-5xl mx-auto px-6 my-6 flex gap-6 text-sm font-medium pt-3">
-                <button
-                    onClick={() => setActiveTab('active')}
-                    className={`pb-3 border-b-2 transition-colors ${activeTab === 'active'
-                        ? 'border-emerald-500 text-emerald-500'
-                        : 'border-transparent text-gray-400 hover:text-gray-200'
-                        }`}
-                >
-                    projects
-                </button>
-                <button
-                    onClick={() => setActiveTab('add')}
-                    className={`pb-3 border-b-2 transition-colors ${activeTab === 'add'
-                        ? 'border-emerald-500 text-emerald-500'
-                        : 'border-transparent text-gray-400 hover:text-gray-200'
-                        }`}
-                >
-                    + add project
-                </button>
-                <button
-                    className="pb-3 border-b-2 border-transparent text-gray-400 hover:text-gray-200 transition-colors"
-                >
-                    console
-                </button>
-            </div>
+        <div className="flex gap-6 h-full text-sm font-medium">
+            <button
+                onClick={() => setActiveTab('active')}
+                className={`h-full border-b-2 transition-colors flex items-center ${activeTab === 'active'
+                    ? 'border-emerald-500 text-emerald-500'
+                    : 'border-transparent text-gray-400 hover:text-gray-200'
+                    }`}
+            >
+                projects
+            </button>
+            <button
+                onClick={() => setActiveTab('add')}
+                className={`h-full border-b-2 transition-colors flex items-center ${activeTab === 'add'
+                    ? 'border-emerald-500 text-emerald-500'
+                    : 'border-transparent text-gray-400 hover:text-gray-200'
+                    }`}
+            >
+                + add project
+            </button>
+            <button
+                className="h-full border-b-2 border-transparent text-gray-400 hover:text-gray-200 transition-colors flex items-center"
+            >
+                console
+            </button>
         </div>
     )
 }
